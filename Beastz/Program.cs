f5bc1913fcbfbdb0e1e7468da2a3855fd8b8f117
Yash Bansod <yashbansod@aurigeneservices.com>
2026-06-12 15:40:02 +0530

    learn: handling the different request methods

--- Beastz/Program.cs
+++ Beastz/Program.cs
@@ -1,13 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/health", (HttpContext context) =>
+app.Run(context =>
 {
     context.Response.StatusCode = 200;
     context.Response.Headers.Server = "Beastz";
     context.Response.ContentType = "text/html";
+
+    Console.WriteLine(context.Request.Path);
+
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("name"))
+        {
+            Console.WriteLine(context.Request.Query["name"]);
+        }
+    }
+
+    if (context.Request.Method == "POST")
+    {
+        StreamReader reader = new(context.Request.Body);
+        string body = reader.ReadToEndAsync().Result;
+        var query = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+        Console.WriteLine(query["name"]);
+    }
     return context.Response.WriteAsync("<h1>Health check passed</h1>");
 });
 
