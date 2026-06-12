720819a720da6786e4a8a1297222be3ad8830407
Yash Bansod <yashbansod@aurigeneservices.com>
2026-06-12 15:05:54 +0530

    learn: how to send response and deal with headers

--- Beastz/Program.cs
+++ Beastz/Program.cs
@@ -3,4 +3,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/health", (HttpContext context) =>
+{
+    context.Response.StatusCode = 200;
+    context.Response.Headers.Server = "Beastz";
+    context.Response.ContentType = "text/html";
+    return context.Response.WriteAsync("<h1>Health check passed</h1>");
+});
+
 app.Run();
