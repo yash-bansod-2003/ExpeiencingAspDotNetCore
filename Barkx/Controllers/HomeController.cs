465d0ac68418dcd76b5bb5e058e8a4c5c8dd9d87
Yash Bansod <yashbansod@aurigeneservices.com>
2026-06-13 22:34:52 +0530

    feat: add Services page and update navigation in layout

--- Barkx/Controllers/HomeController.cs
+++ Barkx/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Services()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
